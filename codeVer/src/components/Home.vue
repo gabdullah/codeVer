e6320ae94592d0cb90dde02b7939c18e2ad10258
Nicholas Naples <naples.nicholas@gmail.com>
2017-10-21 13:26:24 -0400

    Updated

---
+++ codeVer/src/components/Home.vue
@@ -0,0 +1,43 @@
+<template>
+<div id = "container">
+    <codeVerHeader></codeVerHeader>
+
+<p>Enter the sample code below.</p>
+
+<textarea id="codeSample" type="text" v-model="code" placeholder="Sample code here">
+</textarea>
+<br>
+  <button @click="search()">Search</button>
+  <br><br>
+  <button @click="example()">Add sample code(for testing)</button>
+</div>
+</template>
+
+
+
+<script>
+import codeVerHeader from './codeVerHeader.vue'
+export default {
+  name: 'HelloWorld',
+
+  data () {
+    return {
+      code: ''
+    }
+  },
+  components:{
+    codeVerHeader
+  },
+
+  methods: {
+    /* split: function() {
+
+    }
+
+    search: function() {
+
+    } */
+  }
+
+}
+</script>
