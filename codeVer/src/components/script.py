1948f8a8cd5cc90a6d68aff8e1bd742a5bfeff23
gabdullah <gabdull2@kent.edu>
2017-10-22 04:05:51 -0400

    alg

--- codeVer/src/components/script.py
+++ codeVer/src/components/script.py
@@ -10,10 +10,18 @@ def removeWhiteSpace(string):
 	for ch in [' ', '.', ',', ':', ';', '/', '\\', '`', '\'', '\"', '=', '*', '!', '?', '#', '$', '&', '+', '^', '|', '~', '<', '>', '(', ')', '{', '}', '[', ']', '\t', '\n']:
 		if ch in string:
 			string = string.replace(ch, "")
-
 	print string
 	return string
 
+def percentage(inputStrings, dirtyStrings):
+	percents = [1.0,1.0,1.0,1.0,1.0]
+	j = float(len(inputStrings))
+	for i in range(len(dirtyStrings)):
+		percents[i] = float(dirtyStrings[i] / j)
+#		print percents[i]
+		percents[i] = percents[i] * 100.0
+		print percents[i]
+	return percents
 
 
 #for repo in g.search_code('for repo in g.search_code(\'tetris\', language=\'python\'):', language='python'):
@@ -21,16 +29,23 @@ def removeWhiteSpace(string):
 #	print repo.html_url
 #	break
 
-t = ''
-for i in g.get_user().get_repos():
-	t = i
+#t = ''
+#for i in g.get_user().get_repos():
+#	t = i
 #	print t
-	break
+#	break
 
-print t;
+#print t;
 
-testString = "#for repo in g.search_code('for repo in g.search_code(\'tetris\', language=\'python\'):', language='python'):"
-testString = removeWhiteSpace(testString)
+testDirtyStrings = [1, 2, 3, 4, 5] 
+
+testInputStrings = ["#for repo in g.search_code('for repo in g.search_code(\'tetris\', language=\'python\'):', language='python'):", "test", "test2", "test3", "test4"]
+for i in range(len(testInputStrings)):
+	testInputStrings[i] = removeWhiteSpace(testInputStrings[i])
+
+percents = percentage(testInputStrings, testDirtyStrings)
+
+
 
 #g.load(t)
 
