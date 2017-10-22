098920697c0841c89adad124130dd07428700b5f
Nicholas Naples <naples.nicholas@gmail.com>
2017-10-22 02:11:12 -0400

    fixing conflict

--- codeVer/src/components/Home.vue
+++ codeVer/src/components/Home.vue
@@ -61,13 +61,85 @@ export default {
   components:{
     codeVerHeader
   },
+
 methods: {
+
+ methods: {
+    splitter: function(code) {
+      console.log('split called');
+      var lines = code.split('\n');
+      //console.log(lines[0]);
+
+     return lines
+    },
+
+   search: function() {
+      /*const concat = require("concat-stream");
+      const FormData = require('form-data');
+      const fd = new FormData();
+      const fs = require('fs');
+
+     fd.append("-u", fs.createReadStream("gabdullah"));
+      fd.pipe(concat({encoding: 'buffer'}, data => {
+        axios.post("https://api.github.com", data)
+      }));*/
+
+     //var curl = require('curlrequest');
+      //var github = require('octonode');
+
+
+
+
+
+
+     console.log('search called');
+      var code = document.getElementById('textbox').value;
+      var lines = this.splitter(this.code);
+      var counter = 0;
+
+     //while (counter < lines.length){
+      //  console.log("text: " + lines[counter]);
+      //  counter++;
+      //}
+
+    /* return axios.post('https://api.github.com/user', {
+        header: 'Accept: application/vnd.github.v3+json',
+        username: 'gabdullah'
+      })*/
+
+     var gh = new Github({
+        username: 'collinw9898',
+        password: 'Cw122198!'
+      });
+
+     var me = gh.getUser();
+
+     var code = "for (int i = 0; i < 10; i++)";
+    console.log(gh.search({q: code}));
+
+     var posts;
+      var errors = [];
+      return axios.get('https://api.github.com/users/gabdullah/repos')
+      .then(response => {
+        //var data = response.data
+        //var meta = response.meta;
+        //console.log(response.data)
+        //console.log(response.meta);
+      })
+      .catch(e => {
+        errors.push(e)
+        //console.log(errors.pop())
+      })
+
+     //while (counter < posts.length){
+      //  counsole.log(counter + posts[counter]);
+      //  counter++;
+      //}
+
+     //this.$router.push('/final');
+     
   example: function() {
     this.code = "//This is a sample of our code\n<template>\n        <div id=\"heading\">\n          <router-link to=\"/\" class=\"routerLink\">\n            <img src=\"../assets/sineWave.png\" class=\"sine\"/>\n            <img src=\"../assets/flipSineWave.png\" class=\"cosine\"/>\n            <img src=\"../assets/block.png\" class=\"block\"/>\n            <img src=\"../assets/codeVerLogo.png\" class=\"logo\"/>\n            <h1>codeVer</h1>\n            <h2>Your premium code verification software.</h2>\n          </router-link>\n        </div>\n      </template>"
-  },
-    search: function() {
-        this.$router.push('/Final');
-    }
   }
 }
 </script>
