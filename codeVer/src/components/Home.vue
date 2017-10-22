6633390c904f20492d3263671f82174bed097df5
Nicholas Naples <naples.nicholas@gmail.com>
2017-10-22 02:12:32 -0400

    fixing conflict

--- codeVer/src/components/Home.vue
+++ codeVer/src/components/Home.vue
@@ -62,8 +62,6 @@ export default {
     codeVerHeader
   },
 
-methods: {
-
  methods: {
     splitter: function(code) {
       console.log('split called');
@@ -137,7 +135,7 @@ methods: {
       //}
 
      //this.$router.push('/final');
-     
+
   example: function() {
     this.code = "//This is a sample of our code\n<template>\n        <div id=\"heading\">\n          <router-link to=\"/\" class=\"routerLink\">\n            <img src=\"../assets/sineWave.png\" class=\"sine\"/>\n            <img src=\"../assets/flipSineWave.png\" class=\"cosine\"/>\n            <img src=\"../assets/block.png\" class=\"block\"/>\n            <img src=\"../assets/codeVerLogo.png\" class=\"logo\"/>\n            <h1>codeVer</h1>\n            <h2>Your premium code verification software.</h2>\n          </router-link>\n        </div>\n      </template>"
   }
